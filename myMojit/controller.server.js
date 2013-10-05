ae2678c1982b4c3d07bf9ac13a9905c122b9022e
Shukla <shukla@ubuntu.ubuntu-domain>
2013-10-05 20:51:57 +0530

    first commit

---
+++ myMojit/controller.server.js
@@ -0,0 +1,40 @@
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('myMojit', function(Y, NAME) {
+
+/**
+ * The myMojit module.
+ *
+ *  @module myMojit
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.namespace('mojito.controllers')[NAME] = {
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            ac.models.get('myMojitModel').getData(function(err, data ) {
+                if (err) {
+                    ac.error(err);
+                    return;
+                }
+                ac.assets.addCss('./index.css');
+                ac.done({
+                    status: 'Mojito is working wel.',
+                    data: data
+                });
+            });
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-assets-addon', 'mojito-models-addon', 'myMojitModel']});
